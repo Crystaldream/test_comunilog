a5a44878dea14ba06e911fc3f8cca8124fab47cb
Telmo <telmo.reinas@gmail.com>
2024-06-24 17:05:48 +0100

    Exemplo commit 1

---
+++ main.rs
@@ -0,0 +1,17 @@
+
+fn soma(a: i32, b: i32) -> i32 {
+    
+    a + b
+
+}
+
+fn main() {
+
+    let x = 5;
+    let y = 10;
+
+    let resultado = soma(x, y);
+
+    println!("A soma de {} e {} é {}", x, y, resultado);
+
+}
