4f2a3c9b54d24c59176b15f2aa5f879472d04587
Telmo <telmo.reinas@gmail.com>
2024-06-24 17:22:11 +0100

    Commit 2

--- main.rs
+++ main.rs
@@ -7,11 +7,12 @@ fn soma(a: i32, b: i32) -> i32 {
 
 fn main() {
 
-    let x = 5;
-    let y = 10;
+    let x = 10;
+    let y = 20;
 
     let resultado = soma(x, y);
 
     println!("A soma de {} e {} é {}", x, y, resultado);
 
 }
+
